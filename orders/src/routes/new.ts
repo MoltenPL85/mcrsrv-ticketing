f7aba7dfc1019a28bd58eeba57e5de728a8d76bd
MoltenPL85 <bugaev.maxim.85@gmail.com>
2021-05-22 11:42:58 +0500

    Orders Service events with tests

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,6 +10,8 @@ import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import { Ticket } from '../models/ticket';
 import { Order } from '../models/order';
+import { natsWrapper } from '../nats-wrapper';
+import { OrderCreatedPublisher } from '../events/publishers/order-created-publisher';
 
 const router = express.Router();
 
@@ -48,6 +50,18 @@ router.post(
       ticket,
     });
     await order.save();
+
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+      version: 1,
+    });
 
     res.status(201).send(order);
   }
